59deb48012d5d523b6d98b709deb602dec68ac32
omarhesham7 <omarabouhussein@hotmail.com>
2018-10-24 19:22:32 -0400

    Almost done with the item object

---
+++ Omar's/Item.h
@@ -0,0 +1,35 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "rapidxml-1.13/rapidxml.hpp"
+#include "rapidxml-1.13/rapidxml_utils.hpp"
+#include "rapidxml-1.13/rapidxml_print.hpp"
+#include "Trigger.h"
+// using namespace std;
+
+typedef struct _TurnOn{
+  vector <string> toDoActions;
+  string print;
+}TurnOn;
+
+class Item {
+public:
+   string name;
+   string status;
+   string writing;
+   string description;
+   vector <Trigger*> trigger;
+   TurnOn turnOn;
+   int turnOnStatus;
+   virtual ~Item();
+   Item(xml_node<>* node);
+
+private:
+  void declareItem(xml_node<>* node);
+};
+
+
+#endif /* ITEM_H */
