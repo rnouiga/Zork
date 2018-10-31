71d3a6364ab33d8b431803df1da25aaeec80aa1a
eadu <39714813+eadu@users.noreply.github.com>
2018-10-31 14:39:24 -0400

    Update Container.cpp

--- Container.cpp
+++ Container.cpp
@@ -9,31 +9,34 @@ Container::Container(xml_node<>* node)
 
 void Container::makeContainer(node)
 {
-	child = node -> 
-	if(!strcmp(child -> name(), "name"))
+	child = node -> node -> frist_node();
+	while(child)
+	{
+		if(!strcmp(child -> name(), "name"))
 		{
-			name = child ->;
+			name = child -> value();
 		}
 		if(!strcmp(child -> name(), "status"))
 		{
-			status = child -> ;
+			status = child -> value();
 		}
 		if(!strcmp(child -> name(), "description"))
 		{
-			description = child -> ;
+			description = child -> value();
 		}
 		if(!strcmp(child -> name(), "accept"))
 		{
-			accept.push_back();
+			accept.push_back(child -> value());
 		}
 		if(!strcmp(child -> name(), "item"))
 		{
-			item.push_back();
+			item.push_back(child -> value());
 		}
 		if(!strcmp(child -> name(), "trigger"))
 		{
 			Trigger* newTrigger = new Trigger(child);
 			tigger.push_back(newTrigger);
 		}
-		child = child -> next;
+		child = child -> next();
+	}
 }
